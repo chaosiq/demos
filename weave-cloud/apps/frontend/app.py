5ebe67b32e32e0da0698eaaece008a0acc71f889
Sylvain Hellegouarch <sh@defuze.org>
2018-02-21 20:03:21 +0100

    manage db loss
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- weave-cloud/apps/frontend/app.py
+++ weave-cloud/apps/frontend/app.py
@@ -7,8 +7,9 @@ from flask import abort, Flask, render_template, request
 from flask_opentracing import FlaskTracer
 from jaeger_client import Config, Span
 import opentracing
+from sqlalchemy import exc
 
-from model import Star
+from model import db, Star
 
 
 __all__ = ["frontend_app"]
@@ -55,7 +56,16 @@ def new_span(name: str, parent_span: Span=None):
 @tracer.trace("url_rule")
 def index():
     with new_span("query_stars"):
-        stars = Star.query.filter().all()
+        try:
+            stars = Star.query.filter().all()
+        except exc.OperationalError:
+            cherrypy.log("Connection to database lost, trying a new connection")
+
+            # invalidate current pending query
+            db.session.rollback()
+
+            # will re-create a new connection pool
+            stars = Star.query.filter().all()
 
     return render_template('index.html', stars=stars)
 
