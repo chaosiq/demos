df42cca9ce88ad7ad35c2f92bcda176c58316b61
Sylvain Hellegouarch <sh@defuze.org>
2018-02-21 19:36:05 +0100

    rewind
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- weave-cloud/apps/frontend/app.py
+++ weave-cloud/apps/frontend/app.py
@@ -7,9 +7,8 @@ from flask import abort, Flask, render_template, request
 from flask_opentracing import FlaskTracer
 from jaeger_client import Config, Span
 import opentracing
-from sqlalchemy import exc
 
-from model import db, Star
+from model import Star
 
 
 __all__ = ["frontend_app"]
@@ -56,16 +55,7 @@ def new_span(name: str, parent_span: Span=None):
 @tracer.trace("url_rule")
 def index():
     with new_span("query_stars"):
-        try:
-            stars = Star.query.filter().all()
-        except exc.OperationalError:
-            cherrypy.log("Connection to database lost, trying a new connection")
-
-            # invalidate current pending query
-            db.session.rollback()
-
-            # will re-create a new connection pool
-            stars = Star.query.filter().all()
+        stars = Star.query.filter().all()
 
     return render_template('index.html', stars=stars)
 
