4df5c1623e6778c4c4da7f0a81c31167ab8a3807
Sylvain Hellegouarch <sh@defuze.org>
2018-02-22 15:18:08 +0100

    fix issue
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- weave-cloud/apps/frontend/model.py
+++ weave-cloud/apps/frontend/model.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy as SA
 
 __all__ = ["db", "Star"]
+
+class SQLAlchemy(SA):
+    def apply_pool_defaults(self, app, options):
+        SA.apply_pool_defaults(self, app, options)
+        options["pool_pre_ping"] = True
 
 db = SQLAlchemy()
 
